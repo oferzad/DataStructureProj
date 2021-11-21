0053a16aaa57bf29acc3f7b5925a25f98e155a9f
Ofer Zadikario <ofer.zadikario@gmail.com>
2021-11-21 21:03:17 +0200

    Add project files.

---
+++ DataStructureProj/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DataStructureCore;
+
+namespace DataStructureProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
